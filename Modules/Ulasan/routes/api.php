db3f7ab16ee5269067717daf80aade1bf516a98f
Riedho-Syafei <109266989+Riedho-Syafei@users.noreply.github.com>
2026-05-18 14:36:32 +0700

    fix dari imbang

--- Modules/Ulasan/routes/api.php
+++ Modules/Ulasan/routes/api.php
@@ -1,8 +1,4 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Ulasan\Http\Controllers\UlasanController;
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('ulasans', UlasanController::class)->names('ulasan');
-});
--- Modules/Ulasan/routes/web.php
+++ Modules/Ulasan/routes/web.php
@@ -8,8 +8,20 @@ use Modules\Ulasan\Livewire\UlasanAdmin;
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/ulasan', function () {
-        return view('ulasan::pages.ulasan-form');
+		return view('ulasan::pages.ulasan-form');
+		// Karena sudah dapat /ulasan MAKA kita dapatkan produk id dari definisi rute "ulasan/{produk_id}"
+
+
     })->name('ulasan.form');
+	// INI contohnya btw
+	Route::get('ulasan/{produk_id}', function($produk_id)
+	{
+		if (!$produk_id)
+		{
+			$produk_id = Produk::first()->id;
+
+		}
+	});
     Route::get('/admin/ulasan', function () {
         return view('ulasan::pages.ulasan-admin');
     })->name('ulasan.admin');
